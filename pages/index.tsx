b495cd61c67c789220e1e2111eda6943dc2fdecf
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-17 00:21:06 +0900

    apply lottiefiles component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout from '@/components/layout';
 import Hero from '@/components/home/hero';
+import Animation from '@/components/home/animation';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <section className="flex min-h-screen flex-col items-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
+          <Animation/>
         </div>
       </section>
     </Layout>
