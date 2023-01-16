48a0bfc13de26721c6e13867edd76bac924e9742
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-16 22:12:45 +0900

    fix tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 }
