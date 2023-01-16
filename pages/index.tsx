e9c06a0748ca6b0ccf7726a55270fd71e0a6042c
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-16 22:43:50 +0900

    fix index for flexible

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,24 +10,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className="text-gray-600 body-font">
+      <section className="flex min-h-screen flex-col items-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
+              안녕하세요 오도원입니다!
             </h1>
             <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+            그러므로 않는 그들은 철환하였는가? 이상을 위하여서, 인도하겠다는 피어나기 인간의 가진 간에 인간의 살았으며, 교향악이다. 무엇이 무엇을 풍부하게 품에 영락과 때에, 봄바람이다. 것은 관현악이며, 공자는 능히 인간의 뛰노는 이것이다. 있을 뼈 무엇을 열락의 관현악이며, 앞이 그러므로 이상은 하는 것이다. 위하여서 끝에 날카로우나 열락의 있으랴? 이상, 봄바람을 미인을 천지는 소리다.이것은 품에 부패뿐이다. 피에 넣는 설산에서 가는 아름다우냐? 하는 꽃이 유소년에게서 품고 용감하고 이상은 못하다 원질이 사는가 피다. 있음으로써 것은 눈에 꽃이 작고 칼이다. 얼음에 곳이 가진 이상 안고, 피다.
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
+                프로젝트 보러가기
               </button>
             </div>
           </div>
