b886e55db3f56accbbd6fd4f611b154d9fbe1cb8
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-17 23:11:04 +0900

    header: fix link

--- components/header.tsx
+++ components/header.tsx
@@ -10,29 +10,40 @@ export default function Header() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">오도원의 포트폴리오</span>
+            <Link href="/" className="mr-5 hover:text-gray-900">
+              <span className="ml-3 text-xl">오도원의 포트폴리오</span>
+            </Link>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/" className="mr-5 hover:text-gray-900">홈</Link>
-            <Link href="/projects" className="mr-5 hover:text-gray-900">프로젝트</Link>
-            <Link href="https://open.kakao.com/o/swzIKoZe" className="mr-5 hover:text-gray-900">연락하기</Link>
+            <Link href="/" className="mr-5 hover:text-gray-900">
+              홈
+            </Link>
+            <Link href="/projects" className="mr-5 hover:text-gray-900">
+              프로젝트
+            </Link>
+            <Link
+              href="https://open.kakao.com/o/swzIKoZe"
+              className="mr-5 hover:text-gray-900"
+            >
+              연락하기
+            </Link>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
             <svg
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               className="w-4 h-4 ml-1"
               viewBox="0 0 24 24"
             >
