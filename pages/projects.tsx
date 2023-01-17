2a881194732e81643229bdf01b361067b01049a7
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-17 23:11:58 +0900

    projects: add header and section info

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,17 @@
-import Layout from "@/components/layout";
+import Layout from '@/components/layout';
+import Head from 'next/head';
 
 export default function Projects() {
-    return (
-        <Layout>
+  return (
+    <Layout>
+      <Head>
+        <title>오도원의 포트폴리오</title>
+        <meta name="description" content="오늘도 즐거운 개발인생!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="flex min-h-screen flex-col items-center text-gray-600 body-font">
         <h1>프로젝트</h1>
-        </Layout>
-    );
+      </section>
+    </Layout>
+  );
 }
