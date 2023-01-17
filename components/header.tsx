0141d1b46ac18675337bdd857ec3e262bc296574
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-17 23:34:07 +0900

    header: fix error by removing nested <a> and <link>

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -21,7 +21,7 @@ export default function Header() {
             <Link href="/" className="mr-5 hover:text-gray-900">
               <span className="ml-3 text-xl">오도원의 포트폴리오</span>
             </Link>
-          </a>
+          </div>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link href="/" className="mr-5 hover:text-gray-900">
               홈
