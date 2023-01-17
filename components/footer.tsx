4fa8c24076dec7adcb9a38d1e77c3a21170d3edb
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-18 00:52:48 +0900

    fix bg-gray-100 position

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <>
-      <footer className="text-gray-600 body-font">
-        <div className="bg-gray-100">
+      <footer className="text-gray-600 bg-gray-100 body-font">
+        <div>
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
               <Link href="/">
