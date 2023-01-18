fdcab11c30caa2eaeccada27edcb3a594206aae5
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-18 22:37:08 +0900

    projects: fetch notion database data

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout';
 import Head from 'next/head';
+import { TOKEN, DATABASE_ID } from '@/config/index';
 
 export default function Projects() {
   return (
@@ -14,4 +15,27 @@ export default function Projects() {
       </section>
     </Layout>
   );
+}
+
+// once at build time
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+
+  return {
+    props: {}, // will be passed to the page component as props
+  };
 }
