3838f417da396a4220be2ee210ff3da9392f2909
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-19 16:23:24 +0900

    project-item: add margin 3

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -4,7 +4,7 @@ type ProjectNameProps = {
 
 export function ProjectItem({ projectName }: ProjectNameProps) {
   return (
-    <div className="p-6 bg-slate-400 rounded-md">
+    <div className="p-6 m-3 bg-slate-400 rounded-md">
       <h1>{projectName}</h1>
     </div>
   );
