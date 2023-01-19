817d20a47642e83f23cbb5829d4e9392bd528aeb
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-20 00:35:23 +0900

    move to ProjectItem type to project-item.tsx

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -1,11 +1,40 @@
-type ProjectNameProps = {
-  projectName: string;
+import Link from 'next/link';
+
+export type ProjectItem = {
+  id: string;
+  url: string;
+  created_time: Date;
+  last_edited_time: Date;
+  cover_url: string;
+  icon?: string;
+  fields: {
+    Tags: {
+      name: string;
+      color: string;
+    }[];
+    Description: string;
+    WorkPeriod: {
+      start?: Date;
+      end?: Date;
+    };
+    Github: string;
+    Name: string;
+  };
 };
 
-export function ProjectItem({ projectName }: ProjectNameProps) {
+type ProjectProps = {
+  projectItem: ProjectItem;
+};
+
+export function ProjectItem({ projectItem }: ProjectProps) {
   return (
-    <div className="p-6 m-3 bg-slate-400 rounded-md">
-      <h1>{projectName}</h1>
+    <div className="p-6 m-3 bg-slate-700 rounded-md">
+      <h1>{projectItem.fields.Name}</h1>
+      {projectItem.fields.Github != 'Private' ? (
+        <Link href={projectItem.fields.Github}>깃허브 바로가기</Link>
+      ) : (
+        <h1>프라이빗 프로젝트</h1>
+      )}
     </div>
   );
 }
