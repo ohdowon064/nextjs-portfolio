9b9ab0408345cb8cb4cf1f9b0daee144910698b8
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-20 01:15:21 +0900

    next.config: formatting

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.notion.so', 'images.unsplash.com', 's3.us-west-2.amazonaws.com']
-  }
-}
+    domains: [
+      'www.notion.so',
+      'images.unsplash.com',
+      's3.us-west-2.amazonaws.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
